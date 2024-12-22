a8a08a79a836a43022fad018406c7470e7cdff71
BitHaider <liamhunter4399@gmail.com>
2024-12-22 19:35:38 +0100

    🐦‍⬛

--- KingVonPasswordManager/Form1.Designer.cs
+++ KingVonPasswordManager/Form1.Designer.cs
@@ -103,7 +103,7 @@
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Image = global::KingVonPasswordManager.Properties.Resources.ad;
+            this.pictureBox1.Image = global::PasswordGenerator.Properties.Resources.ad;
             this.pictureBox1.Location = new System.Drawing.Point(0, 177);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(332, 220);
--- KingVonPasswordManager/Properties/Resources.Designer.cs
+++ KingVonPasswordManager/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace KingVonPasswordManager.Properties {
+namespace PasswordGenerator.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace KingVonPasswordManager.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("KingVonPasswordManager.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PasswordGenerator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
